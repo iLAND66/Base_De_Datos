5feeb4e746241da2f44fccce8f225b015a9ff387
iLAND66 <82420800+iLAND66@users.noreply.github.com>
2024-09-25 20:35:00 -0600

    Create Nombres.R
    Agrege lo que hemos venido trabajando en las clases

---
+++ Nombres.R
@@ -0,0 +1,154 @@
+# Vector de 200 nombres masculinos únicos
+nombres_hombres <- c(
+  "Aaron", "Abel", "Abraham", "Adam", "Adrian", "Aiden", "Alan", "Albert", "Alec", "Alexander",
+  "Alfred", "André", "Andrés", "Angelo", "Anthony", "Antonio", "Arthur", "Asher", "Austin", "Benjamin",
+  "Bernard", "Blake", "Boris", "Brad", "Brandon", "Brian", "Bruce", "Bryan", "Caleb", "Cameron",
+  "Carl", "Carlos", "Charles", "Christian", "Christopher", "Claude", "Clyde", "Colin", "Connor", "Daniel",
+  "David", "Dean", "Dennis", "Dominic", "Dorian", "Douglas", "Dylan", "Edward", "Eli", "Elijah",
+  "Emmanuel", "Eric", "Ethan", "Evan", "Felix", "Fernando", "Francis", "Francisco", "Gabriel", "George",
+  "Gian", "Glen", "Gordon", "Graham", "Grant", "Gregory", "Hank", "Harry", "Henry", "Hugo",
+  "Ian", "Isaac", "Isaiah", "Jack", "Jacob", "James", "Jason", "Jeffrey", "Jeremy", "Jerome",
+  "Jesse", "John", "Jonathan", "Jordan", "Joseph", "Joshua", "Juan", "Julian", "Justin", "Karl",
+  "Keith", "Kevin", "Kirk", "Kyle", "Landon", "Lawrence", "Leo", "Leonard", "Liam", "Logan",
+  "Louis", "Lucas", "Luke", "Malcolm", "Manuel", "Marc", "Marco", "Marcus", "Martin", "Matthew",
+  "Michael", "Miguel", "Miles", "Mitchell", "Morgan", "Nathan", "Neil", "Nicholas", "Noah", "Oliver",
+  "Oscar", "Patrick", "Paul", "Peter", "Philip", "Rafael", "Raymond", "Richard", "Robert", "Rodrigo",
+  "Ronald", "Ross", "Ryan", "Samuel", "Scott", "Sean", "Sergio", "Simon", "Spencer", "Stanley",
+  "Stephen", "Steven", "Stuart", "Thomas", "Timothy", "Tobias", "Tom", "Tony", "Travis", "Trevor",
+  "Tristan", "Victor", "Vincent", "Walter", "Wesley", "William", "Xavier", "Yannick", "Zachary", "Zane",
+  "Adam", "Albert", "Alfred", "Arnold", "Barry", "Bradley", "Brian", "Bruce", "Bryan", "Carl",
+  "Carson", "Casey", "Cody", "Craig", "Derek", "Dustin", "Ethan", "Evan", "Gordon", "Guy",
+  "Ian", "Isaiah", "Ivan", "Jeff", "Jorge", "Julian", "Kevin", "Kurt", "Larry", "Liam",
+  "Mason", "Max", "Micah", "Nolan", "Owen", "Parker", "Paul", "Quinn", "Riley", "Robert"
+)
+
+# Vector de 200 nombres femeninos únicos
+nombres_mujeres <- c(
+  "Ada", "Adele", "Adriana", "Aileen", "Aisha", "Alicia", "Amara", "Amelia", "Ana", "Andrea",
+  "Angela", "Anna", "Ariana", "Astrid", "Aurora", "Beatrice", "Bella", "Bianca", "Blanca", "Brianna",
+  "Camila", "Carla", "Caroline", "Carmen", "Cecilia", "Chloe", "Claire", "Claudia", "Cleo", "Connie",
+  "Daisy", "Daniela", "Daphne", "Diana", "Dolores", "Eleanor", "Elena", "Eliana", "Elizabeth", "Ella",
+  "Emily", "Emma", "Esther", "Eva", "Evelyn", "Faith", "Fiona", "Frances", "Gabriela", "Gemma",
+  "Georgia", "Gianna", "Gina", "Grace", "Greta", "Hailey", "Hannah", "Harper", "Heather", "Helena",
+  "Holly", "Irene", "Isabel", "Isabella", "Ivy", "Jacqueline", "Jade", "Jane", "Jasmine", "Jessica",
+  "Joanna", "Jocelyn", "Julia", "Juliana", "June", "Kara", "Karen", "Katherine", "Katrina", "Kayla",
+  "Kimberly", "Kira", "Kristen", "Laura", "Leah", "Lena", "Lila", "Lily", "Linda", "Lisa",
+  "Louise", "Lucia", "Lucy", "Luna", "Mackenzie", "Madeline", "Madison", "Maggie", "Maria", "Mariana",
+  "Marina", "Martha", "Mary", "Matilda", "Maya", "Melanie", "Mia", "Michelle", "Mila", "Molly",
+  "Naomi", "Natalia", "Nicole", "Nina", "Olivia", "Ophelia", "Paige", "Pamela", "Penelope", "Peyton",
+  "Phoebe", "Priscilla", "Rachel", "Rebecca", "Riley", "Rose", "Ruby", "Samantha", "Sandra", "Sara",
+  "Savannah", "Scarlett", "Serena", "Sienna", "Sofia", "Sophia", "Stella", "Stephanie", "Susanna", "Sydney",
+  "Talia", "Tamara", "Taylor", "Teresa", "Tracy", "Valentina", "Vanessa", "Vera", "Victoria", "Violet",
+  "Wendy", "Willow", "Zara", "Zoe", "Alice", "Brielle", "Cassandra", "Diana", "Eva", "Faye",
+  "Gwendolyn", "Hazel", "Iris", "Janelle", "Jenna", "Joan", "Kelsey", "Lacey", "Lydia", "Maya",
+  "Nadia", "Opal", "Piper", "Quinn", "Renee", "Riley", "Sage", "Tessa", "Vera", "Zoe",
+  "Adeline", "Alexis", "Angela", "Blair", "Brooklyn", "Caitlin", "Carmen", "Celeste", "Clara", "Cora",
+  "Dakota", "Delilah", "Destiny", "Eliana", "Ella", "Emery", "Estelle", "Fiona", "Gemma", "Hannah"
+)
+
+# Vector de 200 apellidos únicos
+apellidos <- c(
+  "Adams", "Allen", "Anderson", "Armstrong", "Baker", "Bates", "Bell", "Bennett", "Benson", "Berg",
+  "Black", "Blake", "Bond", "Booth", "Bowers", "Boyd", "Bradley", "Brooks", "Brown", "Bryant",
+  "Burke", "Burns", "Butler", "Campbell", "Carter", "Chapman", "Clark", "Collins", "Cook", "Cooper",
+  "Cox", "Craig", "Cross", "Curtis", "Davis", "Day", "Dean", "Diaz", "Dixon", "Douglas",
+  "Duncan", "Edwards", "Ellis", "Evans", "Fisher", "Fitzgerald", "Foster", "Fox", "Franklin", "Garcia",
+  "Garrison", "Garvin", "Gibson", "Gill", "Gordon", "Grant", "Gray", "Green", "Griffin", "Hall",
+  "Harris", "Hart", "Hayes", "Henderson", "Hernandez", "Hill", "Holt", "Howard", "Hughes", "Hunt",
+  "Jackson", "James", "Jenkins", "Johnson", "Jones", "Jordan", "Kelly", "King", "Knight", "Lee",
+  "Lewis", "Long", "Martin", "Mason", "Matthews", "Meyer", "Miller", "Mitchell", "Moore", "Morgan",
+  "Morris", "Murphy", "Murray", "Nelson", "Nguyen", "Nichols", "Norris", "O'Brien", "Olson", "Parker",
+  "Patterson", "Payne", "Perez", "Perry", "Peterson", "Phillips", "Powell", "Price", "Quinn", "Ramirez",
+  "Reed", "Richards", "Richardson", "Riley", "Roberts", "Robertson", "Robinson", "Rodriguez", "Rogers", "Rose",
+  "Russell", "Sanders", "Scott", "Shaw", "Shelton", "Simmons", "Smith", "Stewart", "Stone", "Sullivan",
+  "Taylor", "Thomas", "Thompson", "Torres", "Turner", "Walker", "Walsh", "Ward", "Watson", "Webb",
+  "White", "Williams", "Wilson", "Wright", "Young", "Zimmerman", "Andrews", "Arnold", "Beck", "Blair",
+  "Benson", "Bradshaw", "Brady", "Carroll", "Clarkson", "Craig", "Dalton", "Davis", "Dixon", "Dodd",
+  "Donnelly", "Drake", "Duke", "Dunn", "Ellis", "Farrell", "Fisher", "Floyd", "Forbes", "Freeman",
+  "Gibson", "Greene", "Grady", "Graham", "Griffith", "Harrison", "Hayes", "Henderson", "Hoffman", "Holland",
+  "Hughes", "Jameson", "Jefferson", "Johnston", "Jones", "Kennedy", "Kim", "Lane", "Lawson", "Lee",
+  "Lloyd", "Marshall", "Mason", "Mathews", "McCarthy", "McDonald", "McGee", "Moore", "Morgan", "Morris"
+)
+#nombres completos de hombres
+n <- 100000
+v_nombres_masc1 <- sample(nombres_hombres, n, replace = T)
+v_nombres_masc2 <- sample(nombres_hombres, n, replace = T)
+v_apellidos_masc1 <- sample(apellidos, n, replace = T)
+vacios <- rep("", round(length(nombres_hombres)/6))
+n_vacios <- length(vacios)
+n_h_vac <- c(nombres_hombres, vacios)
+v_nombres_masc2 <- sample(n_h_vac, n, replace = T)
+v_apellidos_masc2 <- sample(apellidos, n, replace = T)
+nom_completo_masc <- paste(v_nombres_masc1, v_nombres_masc2,
+                           v_apellidos_masc1, v_apellidos_masc2)
+
+#nombres completos de mentirosotas
+v_nombres_fem1 <- sample(nombres_mujeres, n, replace = T)
+v_nombres_fem2 <- sample(nombres_mujeres, n, replace = T)
+v_apellidos_fem1 <- sample(apellidos, n, replace = T)
+vacios2 <- rep("", round(length(nombres_mujeres)/6))
+n_vacios2 <- length(vacios2)
+n_m_vac <- c(nombres_mujeres, vacios2)
+v_nombres_fem2 <- sample(n_m_vac, n, replace = T)
+v_apellidos_fem2 <- sample(apellidos, n, replace = T)
+nom_completo_fem <- paste(v_nombres_fem1, v_nombres_fem2,
+                          v_apellidos_fem1, v_apellidos_fem2)
+
+#Eliminar dobles espacios
+nom_completo_masc_lim <- gsub(" +", " ", nom_completo_masc)
+nom_completo_fem_lim <- gsub(" +", " ", nom_completo_fem)
+
+
+#primera letra del nombre + apellido + primera letra del seg apellido
+#funcion para crear correos
+crear_correo <- function(nombre_completo) {
+  partes <- strsplit(nombre_completo, " ")[[1]]
+  primer_nombre <- partes[1]
+  primer_apellido <- partes[length(partes)-1]
+  segundo_apellido <- partes[length(partes)]
+  correo <- paste0(substr(primer_nombre, 1, 1),
+                   primer_apellido,
+                   substr(segundo_apellido, 1, 1),
+                   "@ipn.mx")
+  correo <- tolower(correo)
+  return(correo)
+}
+
+correos_masc <- sapply(nom_completo_masc_lim, crear_correo)
+correos_fem <- sapply(nom_completo_fem_lim, crear_correo)
+
+genero_masc <- rep("Hombre", length(correos_masc))
+genero_fem <- rep("Mujer", length(correos_fem))
+
+contagios <- sample(c(TRUE, FALSE), n, replace = T, prob = c(0.7,1))
+
+result_contaguio <- ifelse(contagios, "Infectado", "Sano")
+
+df_correos <- data.frame(
+  Nombre = c(nom_completo_masc_lim, nom_completo_fem_lim),
+  Genero = c(genero_masc, genero_fem),
+  Correo = c(correos_masc, correos_fem),
+  Contagio = c(result_contaguio)
+)
+
+tasa_contagios <- 0.2
+poblacion_total <- nrow(df_correos)
+
+propagacion <- function(df, tasa_contagios){
+  dias <- 0
+  while(sum(df$Contagio == "Infectado") < nrow(df)){
+    nuevos_infectados <- sum(df$Contagio == "Infectado")
+    sanos <- which(df$Contagio == "Sano")
+    if(nuevos_infectados > length(sanos)){
+      nuevos_infectados <- length(sanos)
+    }
+    a_infectar <- sample(sanos, nuevos_infectados)
+    df$Contagio[a_infectar] <- "Infectado"
+    dias <- dias + 1
+  }
+  return(dias)
+}
+
+tiempo_total <- propagacion(df_correos, tasa_contagios)
+
+print(paste("La poblacion total se infecto en: ", tiempo_total, "dias"))
