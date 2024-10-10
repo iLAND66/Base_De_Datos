54fe1c3a75b2fea531411d27a49d9b164c1645d5
Luis Angel Perez Ortiz <ruisu_3912@hotmail.com>
2024-10-09 22:59:47 -0600

    Avance para pasar con 10

---
+++ Examen.R
@@ -0,0 +1,135 @@
+# Vector de 200 nombres masculinos únicos
+nombres_hombres <- c(
+  "Aaron", "Abel", "Abraham", "Adam", "Adrian", "Aiden", "Alan", "Albert", "Alec", "Alexander",
+  "Alfred", "Andre", "Andres", "Angelo", "Anthony", "Antonio", "Arthur", "Asher", "Austin", "Benjamin",
+  "Bernard", "Blake", "Boris", "Brad", "Brandon", "Brian", "Bruce", "Bryan", "Caleb", "Cameron",
+  "Carl", "Carlos", "Charles", "Christian", "Christopher", "Claude", "Clyde", "Colin", "Connor", "Daniel",
+  "David", "Dean", "Dennis", "Dominic", "Dorian", "Douglas", "Dylan", "Edward", "Eli", "Elijah",
+  "Emmanuel", "Eric", "Ethan", "Evan", "Felix", "Fernando", "Francis", "Francisco", "Gabriel", "George",
+  "Gian", "Glen", "Gordon", "Graham", "Grant", "Gregory", "Hank", "Harry", "Henry", "Hugo",
+  "Ian", "Isaac", "Isaiah", "Jack", "Jacob", "James", "Jason", "Jeffrey", "Jeremy", "Jerome",
+  "Jesse", "John", "Jonathan", "Jordan", "Joseph", "Joshua", "Juan", "Julian", "Justin", "Karl",
+  "Keith", "Kevin", "Kirk", "Kyle", "Landon", "Lawrence", "Leo", "Leonard", "Liam", "Logan",
+  "Louis", "Lucas", "Luke", "Malcolm", "Manuel", "Marc", "Marco", "Marcus", "Martin", "Matthew",
+  "Michael", "Miguel", "Miles", "Mitchell", "Morgan", "Nathan", "Neil", "Nicholas", "Noah", "Oliver",
+  "Oscar", "Patrick", "Paul", "Peter", "Philip", "Rafael", "Raymond", "Richard", "Robert", "Rodrigo",
+  "Ronald", "Ross", "Ryan", "Samuel", "Scott", "Sean", "Sergio", "Simon", "Spencer", "Stanley",
+  "Stephen", "Steven", "Stuart", "Thomas", "Timothy", "Tobias", "Tom", "Tony", "Travis", "Trevor",
+  "Tristan", "Victor", "Vincent", "Walter", "Wesley", "William", "Xavier", "Yannick", "Zachary", "Zane",
+  "Adam", "Albert", "Alfred", "Arnold", "Barry", "Bradley", "Brian", "Bruce", "Bryan", "Carl",
+  "Carson", "Casey", "Cody", "Craig", "Derek", "Dustin", "Ethan", "Evan", "Gordon", "Guy",
+  "Ian", "Isaiah", "Ivan", "Jeff", "Jorge", "Julian", "Kevin", "Kurt", "Larry", "Liam",
+  "Mason", "Max", "Micah", "Nolan", "Owen", "Parker", "Paul", "Quinn", "Riley", "Robert"
+)
+
+# Vector de 200 nombres femeninos únicos
+nombres_mujeres <- c(
+  "Ada", "Adele", "Adriana", "Aileen", "Aisha", "Alicia", "Amara", "Amelia", "Ana", "Andrea",
+  "Angela", "Anna", "Ariana", "Astrid", "Aurora", "Beatrice", "Bella", "Bianca", "Blanca", "Brianna",
+  "Camila", "Carla", "Caroline", "Carmen", "Cecilia", "Chloe", "Claire", "Claudia", "Cleo", "Connie",
+  "Daisy", "Daniela", "Daphne", "Diana", "Dolores", "Eleanor", "Elena", "Eliana", "Elizabeth", "Ella",
+  "Emily", "Emma", "Esther", "Eva", "Evelyn", "Faith", "Fiona", "Frances", "Gabriela", "Gemma",
+  "Georgia", "Gianna", "Gina", "Grace", "Greta", "Hailey", "Hannah", "Harper", "Heather", "Helena",
+  "Holly", "Irene", "Isabel", "Isabella", "Ivy", "Jacqueline", "Jade", "Jane", "Jasmine", "Jessica",
+  "Joanna", "Jocelyn", "Julia", "Juliana", "June", "Kara", "Karen", "Katherine", "Katrina", "Kayla",
+  "Kimberly", "Kira", "Kristen", "Laura", "Leah", "Lena", "Lila", "Lily", "Linda", "Lisa",
+  "Louise", "Lucia", "Lucy", "Luna", "Mackenzie", "Madeline", "Madison", "Maggie", "Maria", "Mariana",
+  "Marina", "Martha", "Mary", "Matilda", "Maya", "Melanie", "Mia", "Michelle", "Mila", "Molly",
+  "Naomi", "Natalia", "Nicole", "Nina", "Olivia", "Ophelia", "Paige", "Pamela", "Penelope", "Peyton",
+  "Phoebe", "Priscilla", "Rachel", "Rebecca", "Riley", "Rose", "Ruby", "Samantha", "Sandra", "Sara",
+  "Savannah", "Scarlett", "Serena", "Sienna", "Sofia", "Sophia", "Stella", "Stephanie", "Susanna", "Sydney",
+  "Talia", "Tamara", "Taylor", "Teresa", "Tracy", "Valentina", "Vanessa", "Vera", "Victoria", "Violet",
+  "Wendy", "Willow", "Zara", "Zoe", "Alice", "Brielle", "Cassandra", "Diana", "Eva", "Faye",
+  "Gwendolyn", "Hazel", "Iris", "Janelle", "Jenna", "Joan", "Kelsey", "Lacey", "Lydia", "Maya",
+  "Nadia", "Opal", "Piper", "Quinn", "Renee", "Riley", "Sage", "Tessa", "Vera", "Zoe",
+  "Adeline", "Alexis", "Angela", "Blair", "Brooklyn", "Caitlin", "Carmen", "Celeste", "Clara", "Cora",
+  "Dakota", "Delilah", "Destiny", "Eliana", "Ella", "Emery", "Estelle", "Fiona", "Gemma", "Hannah"
+)
+
+# Vector de 200 apellidos únicos
+apellidos <- c(
+  "Adams", "Allen", "Anderson", "Armstrong", "Baker", "Bates", "Bell", "Bennett", "Benson", "Berg",
+  "Black", "Blake", "Bond", "Booth", "Bowers", "Boyd", "Bradley", "Brooks", "Brown", "Bryant",
+  "Burke", "Burns", "Butler", "Campbell", "Carter", "Chapman", "Clark", "Collins", "Cook", "Cooper",
+  "Cox", "Craig", "Cross", "Curtis", "Davis", "Day", "Dean", "Diaz", "Dixon", "Douglas",
+  "Duncan", "Edwards", "Ellis", "Evans", "Fisher", "Fitzgerald", "Foster", "Fox", "Franklin", "Garcia",
+  "Garrison", "Garvin", "Gibson", "Gill", "Gordon", "Grant", "Gray", "Green", "Griffin", "Hall",
+  "Harris", "Hart", "Hayes", "Henderson", "Hernandez", "Hill", "Holt", "Howard", "Hughes", "Hunt",
+  "Jackson", "James", "Jenkins", "Johnson", "Jones", "Jordan", "Kelly", "King", "Knight", "Lee",
+  "Lewis", "Long", "Martin", "Mason", "Matthews", "Meyer", "Miller", "Mitchell", "Moore", "Morgan",
+  "Morris", "Murphy", "Murray", "Nelson", "Nguyen", "Nichols", "Norris", "O'Brien", "Olson", "Parker",
+  "Patterson", "Payne", "Perez", "Perry", "Peterson", "Phillips", "Powell", "Price", "Quinn", "Ramirez",
+  "Reed", "Richards", "Richardson", "Riley", "Roberts", "Robertson", "Robinson", "Rodriguez", "Rogers", "Rose",
+  "Russell", "Sanders", "Scott", "Shaw", "Shelton", "Simmons", "Smith", "Stewart", "Stone", "Sullivan",
+  "Taylor", "Thomas", "Thompson", "Torres", "Turner", "Walker", "Walsh", "Ward", "Watson", "Webb",
+  "White", "Williams", "Wilson", "Wright", "Young", "Zimmerman", "Andrews", "Arnold", "Beck", "Blair",
+  "Benson", "Bradshaw", "Brady", "Carroll", "Clarkson", "Craig", "Dalton", "Davis", "Dixon", "Dodd",
+  "Donnelly", "Drake", "Duke", "Dunn", "Ellis", "Farrell", "Fisher", "Floyd", "Forbes", "Freeman",
+  "Gibson", "Greene", "Grady", "Graham", "Griffith", "Harrison", "Hayes", "Henderson", "Hoffman", "Holland",
+  "Hughes", "Jameson", "Jefferson", "Johnston", "Jones", "Kennedy", "Kim", "Lane", "Lawson", "Lee",
+  "Lloyd", "Marshall", "Mason", "Mathews", "McCarthy", "McDonald", "McGee", "Moore", "Morgan", "Morris"
+)
+
+n <- 10
+
+nombreCompleto <- function(nombres_hombres, nombres_mujeres, apellidos, n, genero = "Masculino") {
+  # Inicializar la probabilidad de contagio
+  probabilidad <- 0.9
+
+  # Inicializar el data frame
+  dfNombres <- data.frame(Nombre = character(n),
+                          Correo = character(n),
+                          Genero = character(n),
+                          Infectado = FALSE,
+                          stringsAsFactors = FALSE)
+
+  # Función de contagio anidada
+  contagio <- function(dfNombres, infectados, probabilidad) {
+    if (infectados >= nrow(dfNombres)) {
+      return(dfNombres)
+    } else {
+      cat(paste0("|", dfNombres$Infectado, "|\n"), "|-----|\n \n")
+
+      if (runif(1) < probabilidad && !dfNombres$Infectado[infectados + 1]) {
+        dfNombres$Infectado[infectados + 1] <- TRUE
+      }
+      Sys.sleep(1)
+      return(contagio(dfNombres, infectados + 1, probabilidad))
+    }
+  } # fin contagio
+
+  # Selección de nombres según el género
+  if (genero == "Masculino") {
+    nombres <- nombres_hombres
+  } else if (genero == "Femenino") {
+    nombres <- nombres_mujeres
+  } else {
+    stop("género no válido. Debe ser Masculino o Femenino.")
+  }
+
+  # Generación de nombres y apellidos
+  for (i in 1:n) {
+    dfNombres$Nombre[i] <- paste(sample(nombres, 2), collapse = " ")  # Dos nombres
+    dfNombres$PrimerApellido[i] <- sample(apellidos, 1)
+    dfNombres$SegundoApellido[i] <- sample(apellidos, 1)
+    dfNombres$Correo[i] <- tolower(paste0(substr(dfNombres$Nombre[i], 1, 1),
+                                           dfNombres$PrimerApellido[i],
+                                           substr(dfNombres$SegundoApellido[i], 1, 1),
+                                           "@ipn.mx"))
+  }
+
+  # Iniciar contagio desde la primera persona
+  dfNombres <- contagio(dfNombres, 0, probabilidad)
+
+  return(dfNombres)
+} # fin nombreCompleto
+
+nombresHombresCompletos <- nombreCompleto(nombres_hombres, apellidos, nombres_mujeres, n, "Masculino")
+nombresMujeresCompletos <- nombreCompleto(nombres_hombres, nombres_mujeres, apellidos, n, "Femenino")
+head(nombresHombresCompletos)
+head(nombresMujeresCompletos)
+head(nombreCompleto(dfNombres))
+baseDeDatos <- contagio(nombresHombresCompletos, probabilidad)
+cat(paste0("|",baseDeDatos$Infectado, "|\n"), "|-----|\n \n")
+
+
