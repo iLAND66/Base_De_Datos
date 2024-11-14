d9bd4f2fd353a5914a44986333f3b34089dfbe50
Luis Angel Pérez Ortiz <82420800+iLAND66@users.noreply.github.com>
2024-11-13 20:34:27 -0600

    Update trabajandoSbreTitanic.R
    convirtiendolo en funcion

--- titanic/trabajandoSbreTitanic.R
+++ titanic/trabajandoSbreTitanic.R
@@ -1,4 +1,32 @@
-#dfinimos una tabla usando las columnas pclas y survived
+analisisDeTablas <- function (columna1, columna2) {
+  tabla <- table(columna1,columna2)
+  sumValores <- c(sum(tabla[1,]),sum(tabla[2,]),sum(tabla[3,]))
+  probabilidad <- tabla/sumValores
+  if (identical(columna2, train$Survived)) {
+    tablaSobrevivientes <- table(test.survived$Pclass, test.survived$Survived)
+    tablaSobrevivientes[1]
+    valores <- 0:1
+    print(sample(valores,tablaSobrevivientes[1],replace = T,prob =probabilidad[1,]))
+    cat("\n")
+    print(sample(valores,tablaSobrevivientes[2],replace = T,prob =probabilidad[2,]))
+    cat("\n")
+    print(sample(valores,tablaSobrevivientes[3],replace = T,prob =probabilidad[3,]))
+  } else {
+    valores <- 0:1
+    print(sample(valores, tabla[1], replace = T, prob = probabilidad[1,]))
+    cat("\n")
+    print(sample(valores, tabla[2], replace = T, prob = probabilidad[2,]))
+    cat("\n")
+    print(sample(valores, tabla[3], replace = T, prob = probabilidad[3,]))
+  }
+}
+
+analisisDeTablas(train$Pclass, train$Survived)
+analisisDeTablas(train$Pclass, train$Sex)
+
+
+
+#definimos una tabla usando las columnas pclas y survived
 tabla <- table(train$Pclass,train$Survived)
 #creamos un vector que va a sumar las filas de los valores de nuestra tabla
 sumValores <- c(sum(tabla[1,]),sum(tabla[2,]),sum(tabla[3,]))
